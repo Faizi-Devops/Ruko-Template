e332825012fa0827cd14357f298124a81cff06a4
Faizan Ali <devfaizi435@gmail.com>
2023-03-08 21:33:17 -0800

    new

---
+++ pages/Blog.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/Blog.module.css';
+const Blog = () =>{
+    return(
+        <div className={styles.backgroundall}>
+           <div>
+            <h1 className={styles.mainHeading}>Our Blog.</h1>
+           </div>
+            
+
+
+        </div>
+    )
+}
+export default Blog;
