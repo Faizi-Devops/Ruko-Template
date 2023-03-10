f733921395bcb4dff84047efa403beb1644ad5b3
Faizan Ali <devfaizi435@gmail.com>
2023-03-09 21:03:07 -0800

    changing blog

--- pages/Blog.tsx
+++ pages/Blog.tsx
@@ -36,9 +36,9 @@ const Blog = () => {
                 />
 
 
-                <p className={` mt-3 ${styles.paraone}`} data-aos="fade-right">07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
-                <p className={`${styles.paratwo}`} data-aos="fade-up-right">Build a Beautiful Blog With Ease</p>
-                <p className={`${styles.parathree}`} data-aos="fade-left">My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
+                <p className={` mt-3 ${styles.paraone}`} >07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
+                <p className={`${styles.paratwo}`} >Build a Beautiful Blog With Ease</p>
+                <p className={`${styles.parathree}`} >My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
                 <div>
                     <button type="button" className={`btn ${styles.bootbutton}`}>Read More</button>
                 </div>
@@ -52,9 +52,9 @@ const Blog = () => {
                         height={500}
                         style={{ width: "100%", height: "auto" }}
                     />
-                    <p className={` mt-3 ${styles.paraone}`} data-aos="fade-right">07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
-                    <p className={`${styles.paratwo}`} data-aos="fade-up-right">Build a Beautiful Blog With Ease</p>
-                    <p className={`${styles.parathree}`} data-aos="fade-left">My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
+                    <p className={` mt-3 ${styles.paraone}`} >07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
+                    <p className={`${styles.paratwo}`} >Build a Beautiful Blog With Ease</p>
+                    <p className={`${styles.parathree}`} >My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
 
 
 
@@ -70,9 +70,9 @@ const Blog = () => {
                         height={500}
                         style={{ width: "100%", height: "auto" }}
                     />
-                    <p className={` mt-3 ${styles.paraone} `} data-aos="fade-right">07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
-                    <p className={`${styles.paratwo}`} data-aos="fade-up-right">Build a Beautiful Blog With Ease</p>
-                    <p className={`${styles.parathree}`} data-aos="fade-left">My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
+                    <p className={` mt-3 ${styles.paraone} `} >07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
+                    <p className={`${styles.paratwo}`} >Build a Beautiful Blog With Ease</p>
+                    <p className={`${styles.parathree}`} >My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
 
 
 
@@ -88,9 +88,9 @@ const Blog = () => {
                         height={500}
                         style={{ width: "100%", height: "auto" }}
                     />
-                    <p className={` mt-3 ${styles.paraone}`} data-aos="fade-right">07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
-                    <p className={`${styles.paratwo}`} data-aos="fade-up-right">Build a Beautiful Blog With Ease</p>
-                    <p className={`${styles.parathree}`} data-aos="fade-left">My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
+                    <p className={` mt-3 ${styles.paraone}`}>07 AUGUST &nbsp;&nbsp; <span style={{ color: "#75dab4" }}>Wordpress - Themeforest - Ruko</span></p>
+                    <p className={`${styles.paratwo}`} >Build a Beautiful Blog With Ease</p>
+                    <p className={`${styles.parathree}`} >My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the <br /> feel and sophistication of its superiority. I like people with a keen mind and at the same time easy to talk to. <br /> These qualities can be combined perfectly natural. However, things like people look miserable, if these <br /> properties are connected.</p>
 
 
 
